5084f6c772ce1966ed34b6546c413a5cd8e0cb3b
Ted Cabeen <ted.cabeen@lscg.ucsb.edu>
2017-10-27 09:44:53 -0700

    Adding RT API calls to get more ticket data

--- lscgbot.rb
+++ lscgbot.rb
@@ -6,6 +6,7 @@ class LSCGBot < SlackRubyBot::Bot
   match(/(\[[0-9]{9}|#\d+)/i) do |client, data, issues|
     puts data.text
     results = []
+    message = '```'
     tomatch = data.text
     
     # Remove links from text, since they're already links, by grabbing everything between < and >
@@ -21,40 +22,40 @@ class LSCGBot < SlackRubyBot::Bot
     	results << i.upcase
     end
     results.uniq.each do |ticket|
-=begin 
-Disabling all of this jira-specific stuff for now
-        url = ENV["JIRA_PREFIX"]+"rest/api/latest/issue/"+ticket
-        direct = ENV["JIRA_PREFIX"]+"browse/"+ticket
-        uri = URI.parse(url)
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = true
-        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-        request = Net::HTTP::Get.new(uri.request_uri)
-        request.basic_auth(ENV["JIRA_USER"],ENV["JIRA_PASS"])
-        response = http.request(request)
-        body = JSON.parse(response.body)
-        if response.code != "404"
-            if response.code == "200"
-                # Check if this is already a link in the message. If so, don't bother sending a link
-                if tomatch.include?(direct)
-                    message = "#{ticket}: #{body['fields']['summary']}\n#{body['fields']['status']['name']} (#{body['fields']['issuetype']['name']})"
-                else
-                    message = "#{ticket}: #{body['fields']['summary']}\n#{direct}\n#{body['fields']['status']['name']} (#{body['fields']['issuetype']['name']})"
-                end
-            else
-                message = direct
-            end
-            client.say(channel: data.channel, text: message)
-        end
-=end
       if ticket =~ /#/
 	ticket = ticket[1..-1]
-        message = 'https://help.chem.ucsb.edu/rt/Ticket/Display.html?id='+ticket
+
+	uripath = 'https://help.chem.ucsb.edu/rt/REST/1.0/ticket/'+ticket+'/show'
+        uri = URI(uripath)
+
+        req = Net::HTTP::Post.new(uri.path)
+        req.set_form_data('user' => ENV["RT_USER"], 'pass' => ENV["RT_PASS"])
+
+        res = Net::HTTP.start(uri.hostname, uri.port,
+          :use_ssl => uri.scheme == 'https',
+          :set_debug_output => $stderr) do |http|
+          http.request(req)
+        end
+
+        case res
+        when Net::HTTPSuccess, Net::HTTPRedirection
+          ticketdata = res.body
+	  puts ticketdata
+	  ticketdata.lines.each do |line|
+            line.match(/^Subject: (.*)/) do |match|
+              message << match[1]+"\n"
+            end
+            line.match(/^Requestors: (.*)/) do |match|
+              message << match[1]+"\n"
+            end
+	  end
+        end
+        message << 'https://help.chem.ucsb.edu/rt/Ticket/Display.html?id='+ticket
       else 
 	ticket = ticket[1..-1]
-        message = 'https://www.lscg.ucsb.edu/helpdesk/tech/editTicket.php?taskid='+ticket
+        message << 'https://www.lscg.ucsb.edu/helpdesk/tech/editTicket.php?taskid='+ticket
       end
+      message << '```'
       client.say(channel: data.channel, text: message)
     end
   end
